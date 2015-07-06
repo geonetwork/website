995d71a020d4c71236bb926a7b71faf3fff047ef
Jose Garcia <jose.garcia@geocat.net>
2015-07-06 15:57:41 +0200

    Update build script to build the user manual for version 3 from https://github.com/geonetwork/doc

--- checkup_docs.sh
+++ checkup_docs.sh
@@ -1,83 +1,71 @@
 #!/bin/bash
 
+export MAVEN_OPTS="-Xmx512M -XX:MaxPermSize=256M"
+
 current_dir=$(pwd)
+
+tag210='2.10.4'
+
+echo "=======> Website creation started"
 
 # -------------------------------------------------------
 # GIT ---------------------------------------------------
 # -------------------------------------------------------
 
-# Download from GitHub master to 'latest' folder
+# Download from GitHub master to 'latest' folder"
+echo "=======> Download from GitHub develop branch to 'latest' folder"
+
 rm -rf latest
 git clone git://github.com/geonetwork/core-geonetwork.git latest
-cd latest
-git submodule update --init
-./git-all checkout master
-cd ..
-#rm -rf `find latest -type d -name .svn`
 
-# Create folder for 2.10.x branch and checkout v2.10.2
+cd $current_dir/latest
+git checkout -b develop --track origin/develop
+git submodule update --init
+
+# Create folder for 2.10.x branch to '210x' folder
+echo "=======> Create folder for 2.10.x branch"
+
+cd $current_dir
 rm -rf 210x
 cp -R latest 210x
-cd 210x
-./git-all checkout -b 2.10.2 remotes/origin/2.10.x
-cd ..
-#rm -rf `find 210x -type d -name .svn`
+cd $current_dir/210x
+git fetch --tags
+git checkout tags/$tag210
+git submodule update --init
+git clean -fxd
 
-# Create folder for 2.6.x branch and checkout v2.6.4
-#rm -rf 26x
-#cp -R latest 26x
-#cd 26x
-#./git-all checkout -b 2.6.4 remotes/origin/2.6.x
-#cd ..
+# Download from GitHub docs to 'doc' folder
+echo "=======> Download from GitHub docs to 'doc' folder"
+cd $current_dir
+rm -rf doc
+git clone https://github.com/geonetwork/doc.git
+
+
 
 # -------------------------------------------------------
-# Trunk documentation -----------------------------------
+# Develop branch documentation ---------------------------
 # -------------------------------------------------------
 
+#echo "=======> Build develop branch javadocs"
 # Build javadoc for trunk
-cd latest/web
-#mvn install
-mvn javadoc:javadoc
+#cd latest
+#mvn clean site
 
-cd ../jeeves
-#mvn install
-mvn javadoc:javadoc
+echo "=======> Build develop branch manuals"
+pwd
+cd $current_dir/doc
+mvn clean install
 
-# Build website docs for trunk
-cd ../docs
-mvn install
 
 # -------------------------------------------------------
 # 2.10.x documentation ----------------------------------
 # -------------------------------------------------------
 
-# Build javadoc for 2.10.x tag
-cd $current_dir/210x/web
-#mvn install
-mvn javadoc:javadoc
-
-cd ../jeeves
-#mvn install
-mvn javadoc:javadoc
-
+echo "=======> Build 2.10.x branch manuals"
 # Build website docs for 2.10.x tag
-cd ../docs
+cd $current_dir/210x/docs
 mvn install
 
-# -------------------------------------------------------
-# 2.6.x documentation ----------------------------------
-# -------------------------------------------------------
-
-# Build javadoc for 2.6.x tag
-#cd $current_dir/26x/web
-#mvn javadoc:javadoc
-
-#cd ../jeeves
-#mvn javadoc:javadoc
-
-# Build website docs for 2.6.x tag
-#cd ../docs
-#mvn install
 
 # -------------------------------------------------------
 # geonetwork-opensource website -------------------------
@@ -85,94 +73,41 @@ mvn install
 
 # Build geonetwork-opensource website
 # -------------------------------------------------------
+echo "=======> Build website"
+
 cd $current_dir/docsrc
 make clean
 make html
 
-# Copy GeoNetwork docs to website folder (trunk)
+# Copy GeoNetwork manuals to website folder (trunk)
 # -------------------------------------------------------
-mkdir -p $current_dir/docsrc/build/html/manuals/trunk/eng/developer/apidocs/geonetwork
-mkdir -p $current_dir/docsrc/build/html/manuals/trunk/eng/developer/apidocs/jeeves
-mkdir $current_dir/docsrc/build/html/manuals/trunk/eng/users
-
-mkdir $current_dir/docsrc/build/html/manuals/trunk/eng/widgets
-mkdir $current_dir/docsrc/build/html/manuals/trunk/fra
-mkdir $current_dir/docsrc/build/html/manuals/trunk/fra/users
+echo "=======> Copy GeoNetwork trunk docs to website folder"
+#mkdir -p $current_dir/docsrc/build/html/manuals/trunk/eng/developer/apidocs/geonetwork
+#mkdir -p $current_dir/docsrc/build/html/manuals/trunk/eng/developer/apidocs/jeeves
+mkdir -p $current_dir/docsrc/build/html/manuals/trunk/eng/users
+mkdir -p $current_dir/docsrc/build/html/manuals/trunk/fra/users
 
 # ... Users
-cd $current_dir/latest/docs/eng/users/build
-cp -R html/* $current_dir/docsrc/build/html/manuals/trunk/eng/users
-cp -R latex/GeoNetworkUserManual.pdf $current_dir/docsrc/build/html/manuals/trunk/eng/users
-cd $current_dir/latest/docs/fra/users/build
-cp -R html/* $current_dir/docsrc/build/html/manuals/trunk/fra/users
+cd $current_dir/doc/target/doc/en/
+cp -R * $current_dir/docsrc/build/html/manuals/trunk/eng/users
+cd $current_dir/doc/target/doc/fr/
+cp -R * $current_dir/docsrc/build/html/manuals/trunk/fra/users
 
-# ... Developer
-cd $current_dir/latest/docs/eng/developer/build
-cp -R html/* $current_dir/docsrc/build/html/manuals/trunk/eng/developer
-cp -R latex/GeoNetworkDeveloperManual.pdf $current_dir/docsrc/build/html/manuals/trunk/eng/developer
-
-# ... Widgets
-cd $current_dir/latest/docs/widgets/build
-cp -R html/* $current_dir/docsrc/build/html/manuals/trunk/eng/widgets
-
-#javadoc
-cd $current_dir/latest/web/target/site
-cp -R apidocs/* $current_dir/docsrc/build/html/manuals/trunk/eng/developer/apidocs/geonetwork
-
-cd $current_dir/latest/jeeves/target/site
-cp -R apidocs/* $current_dir/docsrc/build/html/manuals/trunk/eng/developer/apidocs/jeeves
 
 # Copy docs to website folder (2.10.x)
 # -------------------------------------------------------
-mkdir -p $current_dir/docsrc/build/html/manuals/2.10.2/eng/developer/apidocs/geonetwork
-mkdir -p $current_dir/docsrc/build/html/manuals/2.10.2/eng/developer/apidocs/jeeves
-mkdir $current_dir/docsrc/build/html/manuals/2.10.2/eng/users
-mkdir $current_dir/docsrc/build/html/manuals/2.10.2/eng/widgets
-mkdir $current_dir/docsrc/build/html/manuals/2.10.2/fra
-mkdir $current_dir/docsrc/build/html/manuals/2.10.2/fra/users
+echo "=======> Copy GeoNetwork 2.10.x docs to website folder"
+mkdir -p $current_dir/docsrc/build/html/manuals/$tag210/eng/users
+mkdir -p $current_dir/docsrc/build/html/manuals/$tag210/eng/developer
 
 # ... Users
 cd $current_dir/210x/docs/eng/users/build
-cp -R html/* $current_dir/docsrc/build/html/manuals/2.10.2/eng/users
-cp -R latex/GeoNetworkUserManual.pdf $current_dir/docsrc/build/html/manuals/2.10.2/eng/users
-cd $current_dir/210x/docs/fra/users/build
-cp -R html/* $current_dir/docsrc/build/html/manuals/2.10.2/fra/users
-cp -R latex/GeoNetworkUserManual.pdf $current_dir/docsrc/build/html/manuals/2.10.2/fra/users
+cp -R html/* $current_dir/docsrc/build/html/manuals/$tag210/eng/users
+cp -R latex/GeoNetworkUserManual.pdf $current_dir/docsrc/build/html/manuals/$tag210/eng/users
 
 # ... Developer
 cd $current_dir/210x/docs/eng/developer/build
-cp -R html/* $current_dir/docsrc/build/html/manuals/2.10.2/eng/developer
-cp -R latex/GeoNetworkDeveloperManual.pdf $current_dir/docsrc/build/html/manuals/2.10.2/eng/developer
+cp -R html/* $current_dir/docsrc/build/html/manuals/$tag210/eng/developer
+cp -R latex/GeoNetworkDeveloperManual.pdf $current_dir/docsrc/build/html/manuals/$tag210/eng/developer
 
-# ... Widgets
-cd $current_dir/210x/docs/widgets/build
-cp -R html/* $current_dir/docsrc/build/html/manuals/2.10.2/eng/widgets
-
-#javadoc
-cd $current_dir/210x/web/target/site
-cp -R apidocs/* $current_dir/docsrc/build/html/manuals/2.10.2/eng/developer/apidocs/geonetwork
-cd $current_dir/210x/jeeves/target/site
-cp -R apidocs/* $current_dir/docsrc/build/html/manuals/2.10.2/eng/developer/apidocs/jeeves
-
-# Copy docs to website folder (2.6.4)
-# -------------------------------------------------------
-#mkdir -p $current_dir/docsrc/build/html/manuals/2.6.4/eng/developer/apidocs/geonetwork
-#mkdir -p $current_dir/docsrc/build/html/manuals/2.6.4/eng/developer/apidocs/jeeves
-#mkdir $current_dir/docsrc/build/html/manuals/2.6.4/eng/users
-
-# ... Users
-#cd $current_dir/26x/docs/eng/users/build
-#cp -R html/* $current_dir/docsrc/build/html/manuals/2.6.4/eng/users
-#cp -R latex/GeoNetworkUserManual.pdf $current_dir/docsrc/build/html/manuals/2.6.4/eng/users
-
-# ... Developer
-#cd $current_dir/26x/docs/eng/developer/build
-#cp -R html/* $current_dir/docsrc/build/html/manuals/2.6.4/eng/developer
-#cp -R latex/GeoNetworkDeveloperManual.pdf $current_dir/docsrc/build/html/manuals/2.6.4/eng/developer
-
-#javadoc
-#cd $current_dir/26x/web/target/site
-#cp -R apidocs/* $current_dir/docsrc/build/html/manuals/2.6.4/eng/developer/apidocs/geonetwork
-#cd $current_dir/26x/jeeves/target/site
-#cp -R apidocs/* $current_dir/docsrc/build/html/manuals/2.6.4/eng/developer/apidocs/jeeves
-
+echo "=======> Website creation finished"
